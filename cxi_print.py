d5560426ed26061fb59fd3e1fe66046292528196
Our user <sbobkov@weed1.grid.kiae.ru>
2017-04-10 10:52:37 +0300

    Substitute fixed python path with env discovery

--- cxi_print.py
+++ cxi_print.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # coding: utf-8
 import h5py
 import sys
--- cxi_view.py
+++ cxi_view.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import h5py
 import sys
 from PyQt4 import QtGui, QtCore, Qt
